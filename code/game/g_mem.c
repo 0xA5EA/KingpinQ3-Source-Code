0f11809a0338b46894ed9f4008a46e2a02fa6902
0xA5EA <0xA5EA@kingpinq3.com>
2021-01-05 19:55:42 +0100

    FIX: game server crashes if more than 24 players join the game

--- code/game/g_mem.c
+++ code/game/g_mem.c
@@ -24,7 +24,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "g_local.h"
 
-#define POOLSIZE (256 * 1024)
+#define POOLSIZE (2048 * 2048)
 
 static char memoryPool[POOLSIZE];
 static int allocPoint;
