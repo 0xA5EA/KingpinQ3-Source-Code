01ba6d4bf97ecaac923ed3653d9762d1643bef0e
0xA5EA <0xA5EA@kingpinq3.com>
2021-01-07 12:33:54 +0100

    FIX: kmap2 build on windows

--- kmap2/common/imagelib.c
+++ kmap2/common/imagelib.c
@@ -36,7 +36,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #define JPEG_INTERNALS
-#include "../jpeg/jpeglib.h"
+#include "../jpeg-6/jpeglib.h"
 #include "../../code/png/png.h"
 
 
