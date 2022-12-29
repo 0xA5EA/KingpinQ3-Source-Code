4a1b6b13930e8f9d20362bf6ee8aac221b5f7fd4
0xA5EA <area@puck871.dedicatedpanel.com>
2022-12-29 13:17:39 +0100

    CHG: copy command

--- copybin.sh
+++ copybin.sh
@@ -1,4 +1,4 @@
 
 
-cp build/release-linux-x86_64/kingpinq3.x86_64 ../kpq3/. -v
+cp build/release-linux-x86_64/*.x86_64 ../kpq3/. -v
 cp build/release-linux-x86_64/basekpq3/*.so ../kpq3/basekpq3/. -v
